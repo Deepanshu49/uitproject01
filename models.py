38d20f8274ad71306db7535740619262aeaa832e
Deepanshu Shukla <codexdeepanshu@gmail.com>
2026-03-30 11:58:03 +0530

    update in views and numbers file

--- models.py
+++ models.py
@@ -1,3 +1,6 @@
 from django.db import models
-
-# Create your models here.
+class app1(models.Model):
+    universityrollno=models.IntegerField(unique=True)
+    firstname=models.CharField(max_length=255)
+    lastname=models.CharField(max_length=255)
+    email=models.EmailField(max_length=255,unique=True)
